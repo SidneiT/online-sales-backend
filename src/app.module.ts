53bf9cb111de859dee7e7cac1ce89cf25289bbbc
Sidnei Teixeira <sidnei.teixeira@outlook.com.br>
2024-01-11 18:23:59 -0300

    refactor: change module path, move db connection to a single file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,22 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AddressModule } from './modules/address/address.module';
+import { CityModule } from './modules/city/city.module';
+import { StateModule } from './modules/state/state.module';
+import { UserModule } from './modules/user/user.module';
+import { TypeOrmConnection } from './database/config/connection';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env.development.local'],
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
-      migrations: [`${__dirname}/database/migration/*{.ts,.js}`],
-      migrationsRun: true,
-    }),
+    TypeOrmConnection,
     UserModule,
+    AddressModule,
+    CityModule,
+    StateModule,
   ],
   controllers: [],
   providers: [],
